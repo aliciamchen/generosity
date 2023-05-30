686cc8f8733da08cb1285c5460432c52265dde04
Alicia Chen <31644506+aliciamchen@users.noreply.github.com>
2023-05-30 17:43:58 -0400

    save data alternating repeating logic + attention

--- experiments/tacit_coordination/js/trials.js
+++ experiments/tacit_coordination/js/trials.js
@@ -30,26 +30,23 @@ function makeTrials(condition_number, jsPsych) {
         ],
         data: {
           type: "response",
+          stage: "first",
+          // correct: jsPsych.timelineVariable("first_meeting") === data.response.first_q,
           story: jsPsych.timelineVariable("story"),
           altruistic_status: jsPsych.timelineVariable("altruistic_status"),
+          first_meeting: jsPsych.timelineVariable("first_meeting"),
+          first_actual: jsPsych.timelineVariable("first_actual"),
           vignette: jsPsych.timelineVariable("vignette"),
           answer_labels: {
             alice: jsPsych.timelineVariable("alice"),
             bob: jsPsych.timelineVariable("bob"),
           },
         },
+        on_finish: function (data) {
+          data.correct = jsPsych.timelineVariable("first_meeting") === data.response.first_q
+          console.log(data)
+        },
         button_label: "Submit",
-        //          if (data.story == "attention") {
-        //            if (
-        //              data.response.first_q == "alice" &&
-        //              data.response.symmetric == 0 &&
-        //              data.response.no_relationship == 6
-        //            ) {
-        //              data.passAttentionCheck = true;
-        //            } else {
-        //              data.passAttentionCheck = false;
-        //            }
-        //          }
       },
       // Submitted response
       {
@@ -104,21 +101,26 @@ function makeTrials(condition_number, jsPsych) {
         ],
         data: {
           type: "response",
+          stage: "second",
           story: jsPsych.timelineVariable("story"),
           altruistic_status: jsPsych.timelineVariable("altruistic_status"),
+          first_meeting: jsPsych.timelineVariable("first_meeting"),
+          first_actual: jsPsych.timelineVariable("first_actual"),
           vignette: jsPsych.timelineVariable("vignette"),
           answer_labels: {
             alice: jsPsych.timelineVariable("alice"),
             bob: jsPsych.timelineVariable("bob"),
           },
         },
-
+        button_label: "Submit",
         on_finish: function (data) {
           var curr_progress_bar_value = jsPsych.getProgressBarCompleted();
           jsPsych.setProgressBar(
             curr_progress_bar_value +
               1 / fetchTrialParams(condition_number).length
           );
+          data.strategy = jsPsych.data.get().last(4).values()[0].response.first_q === data.response.second_q ? "repeating" : "alternating"
+          console.log(data)
         },
       },
 
@@ -150,52 +152,155 @@ function makeTrials(condition_number, jsPsych) {
 
   // var attentionParams = fetchAttentionTrialParams();
 
-//  var attentionTrial = {
-//    type: jsPsychSurveyLikert,
-//    preamble: attentionParams.vignette,
-//    questions: [
-//      {
-//        prompt: attentionParams.asymmetric,
-//        name: "asymmetric",
-//        labels: params.likertLabels,
-//      },
-//      {
-//        prompt: attentionParams.symmetric,
-//        name: "symmetric",
-//        labels: params.likertLabels,
-//      },
-//      {
-//        prompt: attentionParams.no_relationship,
-//        name: "no_relationship",
-//        labels: params.likertLabels,
-//      },
-//    ],
-//    data: {
-//      type: "response",
-//      story: attentionParams.story,
-//      social_interaction: attentionParams.social_interaction,
-//      vignette: attentionParams.vignette,
-//      answer_labels: {
-//        asymmetric: attentionParams.asymmetric,
-//        symmetric: attentionParams.symmetric,
-//        no_relationship: attentionParams.no_relationship,
-//      },
-//    },
-//    randomize_question_order: false,
-//    button_label: "Submit",
-//    on_finish: function (data) {
-//      if (
-//        data.response.asymmetric == 6 &&
-//        data.response.symmetric == 0 &&
-//        data.response.no_relationship == 6
-//      ) {
-//        data.passAttentionCheck = true;
-//      } else {
-//        data.passAttentionCheck = false;
-//      }
-//    },
-//  };
-//
-  //return [regularTrials, attentionTrial];
-  return [regularTrials]
+  var attentionTrial = {
+    timeline: [
+      // First question
+      {
+        type: jsPsychSurveyMultiChoice,
+        preamble: function() {
+          var html = `<h2>First time</h2>\
+          <p><span class="vignette">${jsPsych.timelineVariable("vignette")}</span></p><hr>`
+          return html;
+        },
+        questions: [
+          {
+            prompt: function() {
+              var html = `<div class="container"><div class="text-box"><p>${jsPsych.timelineVariable("first_q")}</p></div></div>\
+              `;
+              return html;
+            },
+            name: "first_q",
+            options: function() {
+              var options = [
+                `${jsPsych.timelineVariable("alice")}`,
+                `${jsPsych.timelineVariable("bob")}`,
+              ];
+              return options;
+            },
+            required: true,
+            horizontal: true,
+          },
+        ],
+        data: {
+          type: "attention",
+          stage: "first",
+          // correct: jsPsych.timelineVariable("first_meeting") === data.response.first_q,
+          story: jsPsych.timelineVariable("story"),
+          altruistic_status: jsPsych.timelineVariable("altruistic_status"),
+          first_meeting: jsPsych.timelineVariable("first_meeting"),
+          first_actual: jsPsych.timelineVariable("first_actual"),
+          vignette: jsPsych.timelineVariable("vignette"),
+          answer_labels: {
+            alice: jsPsych.timelineVariable("alice"),
+            bob: jsPsych.timelineVariable("bob"),
+          },
+        },
+        on_finish: function (data) {
+          data.correct = jsPsych.timelineVariable("first_meeting") === data.response.first_q
+          console.log(data)
+        },
+        button_label: "Submit",
+      },
+      // Submitted response
+      {
+        type: jsPsychHtmlKeyboardResponse,
+        stimulus: "Submitting response...",
+        choices: "NO_KEYS",
+        trial_duration: function () {
+          return jsPsych.randomization.sampleWithoutReplacement(
+            [1500, 1750, 2000, 2300],
+            1
+          )[0];
+        },
+      },
+      // Feedback
+      {
+        type: jsPsychHtmlButtonResponse,
+        stimulus: function () {
+          var html = `<h2>First time actual</h2>\
+          <p><span class="vignette">${jsPsych.timelineVariable("vignette")}</span></p>\
+          <div class="container"><div class="text-box">
+          <p><strong>What actually happened the first time: </strong>${jsPsych.timelineVariable("first_actual")}</p></div></div> <br>`
+          return html;
+        },
+        choices: ["Continue"],
+      },
+
+      // Second question
+      {
+        type: jsPsychSurveyMultiChoice,
+        preamble: function() {
+          var html = `<h2>Second time</h2>\
+          <p><span class="vignette">${jsPsych.timelineVariable("vignette")}</span></p>\
+          <hr>`
+          return html;
+        },
+        questions: [
+          {
+            prompt: function() {
+              var html = `<div class="container"><div class="text-box"><p>${jsPsych.timelineVariable("second_q")}</p></div></div>`;
+              return html;
+            }
+            ,
+            name: "second_q",
+            options: [
+              jsPsych.timelineVariable("alice"),
+              jsPsych.timelineVariable("bob"),
+            ],
+            required: true,
+            horizontal: true,
+          },
+        ],
+        data: {
+          type: "attention",
+          stage: "second",
+          story: jsPsych.timelineVariable("story"),
+          altruistic_status: jsPsych.timelineVariable("altruistic_status"),
+          first_meeting: jsPsych.timelineVariable("first_meeting"),
+          first_actual: jsPsych.timelineVariable("first_actual"),
+          vignette: jsPsych.timelineVariable("vignette"),
+          answer_labels: {
+            alice: jsPsych.timelineVariable("alice"),
+            bob: jsPsych.timelineVariable("bob"),
+          },
+        },
+        button_label: "Submit",
+        on_finish: function (data) {
+          var curr_progress_bar_value = jsPsych.getProgressBarCompleted();
+          jsPsych.setProgressBar(
+            curr_progress_bar_value +
+              1 / fetchTrialParams(condition_number).length
+          );
+          data.passAttention = (data.response.second_q == jsPsych.timelineVariable("bob")) && (jsPsych.data.get().last(4).values()[0].response.first_q == jsPsych.timelineVariable("alice"))
+          console.log(data.passAttention)
+        },
+      },
+
+      {
+        type: jsPsychHtmlKeyboardResponse,
+        stimulus: "Thank you for your responses!",
+        choices: "NO_KEYS",
+        trial_duration: function () {
+          return jsPsych.randomization.sampleWithoutReplacement(
+            [1500, 1750, 2000, 2300],
+            1
+          )[0];
+        },
+      },
+    ],
+    timeline_variables:   [{
+        story: "attention",
+        altruistic_status: "",
+        first_meeting: "",
+        vignette: "Thank you for contributing to this study! This page is an attention check so we can make sure you're not a bot and can award you your pay when you complete the study.",
+        first_actual: "Please press continue.",
+        first_q: "Please select \"Alice spent most of the time talking...\" for this question.",
+        alice: "Alice spent most of the time talking, and Angela spent most of the time asking questions.",
+        bob: "Angela spent most of the time talking, and Angela spent most of the time asking questions.",
+        second_q: "Please select \"Angela spent most of the time talking...\" for this question."
+    }],// fetchTrialParams(condition_number),
+  };
+
+  return [regularTrials, attentionTrial];
+  // return [regularTrials]
 }
