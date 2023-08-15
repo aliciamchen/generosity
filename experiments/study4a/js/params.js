bae2a7eaec0e30a5493da6e1f84a7528a0f70013
Alicia Chen <31644506+aliciamchen@users.noreply.github.com>
2023-08-15 17:19:24 -0400

    add study 4a stuff after pilot

---
+++ experiments/study4a/js/params.js
@@ -0,0 +1,7 @@
+const params = {
+    completionMinutes: 20,
+    basePay: '5',
+    likertLabels: ['1 (Forbidden)', '2', '3', '4 (Permissible)', '5', '6', '7 (Obligatory)']
+}
+
+const local_testing = false;
